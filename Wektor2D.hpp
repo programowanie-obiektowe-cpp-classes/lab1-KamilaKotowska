3116db814751b50349602ce080294ac4043dc316
KamilaKotowska <148885436+KamilaKotowska@users.noreply.github.com>
2023-10-28 18:42:36 +0200

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,59 @@
+#include <iostream>
+
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D() 
+    {
+        x = 0;
+        y = 0;
+    }
+    Wektor2D(double a, double b) 
+    {
+        x = a; 
+        y = b;
+        std::cout<<"pobralem wartosc" << std::endl;
+    }
+
+    double norm() {
+        return sqrt(x * x + y * y);
+    }
+    void print() {
+        std::cout << norm() << std::endl;
+    }
+    void setX(double a) 
+    {
+        x = a;
+    }
+    void setY(double b) 
+    {
+        y = b;
+    }
+
+    double getX(double a)
+    {
+        return x;
+    }
+
+    double getY(double b)
+    {
+        return y;
+    }
+
+   // ~Wektor2D()
+    //    void print() {
+    //    std::cout << "destruktor" << std::endl;
+   // }
+private:
+    double x;
+    double y;
 };
+
+Wektor2D operator+(Wektor2D A, Wektor2D B) {
+    return (A.getX() + B.getY());
+}
+
+Wektor2D operator*(Wektor2D A, Wektor2D B) {
+    return (A.getX() * B.getY());
+}
